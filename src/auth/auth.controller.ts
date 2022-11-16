61e526fa9b8d26fa9ab29f0923411dc1f953242e
jhonperez95 <jhonperez.j95@gmail.com>
2022-11-16 00:25:47 -0500

    se crea un decorador para devolver los headers

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -8,10 +8,14 @@ import {
   Delete,
   UseGuards,
   Req,
+  Header,
+  Headers,
 } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { Request } from 'express';
+import { IncomingHttpHeaders } from 'http';
 import { AuthService } from './auth.service';
+import { GetRawHeaders } from './decorator/get-rawHeaders.decorator';
 import { GetUser } from './decorator/get-user.decorator';
 import { CreateUserDto, LoginUserDto, UpdateUserDto } from './dto/';
 import { User } from './entities/user.entity';
@@ -36,14 +40,16 @@ export class AuthController {
     @Req() request: Request,
     @GetUser() user: User,
     @GetUser(['email', 'fullName']) userEmail: User,
+    @GetRawHeaders() rawHeaders: string[],
+    @Headers() headers: IncomingHttpHeaders,
   ) {
-   
-    console.log(request.rawHeaders);
     return {
       ok: true,
       message: 'Route private',
       user,
       userEmail,
+      rawHeaders,
+      headers,
     };
   }
 
