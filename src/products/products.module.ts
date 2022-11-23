53183a529204a62d8e3c6a14729bee27d9d2416e
jhonperez95 <jhonperez.j95@gmail.com>
2022-11-22 23:08:25 -0500

    protegiendo modulo

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -3,11 +3,12 @@ import { ProductsService } from './products.service';
 import { ProductsController } from './products.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Product, ProductImage } from './entities';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
   controllers: [ProductsController],
   exports: [ProductsService, TypeOrmModule],
-  imports: [TypeOrmModule.forFeature([Product, ProductImage])],
+  imports: [TypeOrmModule.forFeature([Product, ProductImage]), AuthModule],
   providers: [ProductsService],
 })
 export class ProductsModule {}
