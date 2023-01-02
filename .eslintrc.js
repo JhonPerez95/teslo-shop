287675b9efabb49f92be8de87f3a3428906e6e37
Jhon Perez <95254043+perezl40@users.noreply.github.com>
2023-01-02 16:03:50 -0500

    se solcuinan problemas

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,6 +8,7 @@ module.exports = {
   plugins: ['@typescript-eslint/eslint-plugin'],
   extends: [
     'plugin:@typescript-eslint/recommended',
+    'google',
     'plugin:prettier/recommended',
   ],
   root: true,
@@ -21,5 +22,20 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    'require-jsdoc': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
+    'no-unused-vars': 'off',
+    'no-console': 'warn',
+    'new-cap': 'off',
+    // '@typescript-eslint/naming-convention': [
+    //   'error',
+
+    //   {
+    //     selector: 'memberLike',
+    //     modifiers: ['private'],
+    //     format: ['camelCase'],
+    //     leadingUnderscore: 'require',
+    //   },
+    // ],
   },
 }
