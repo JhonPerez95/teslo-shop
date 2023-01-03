3bc1e71bc4c1a5d1e5397c09a112234078eaa8bb
Jhon Perez <95254043+perezl40@users.noreply.github.com>
2023-01-03 01:16:44 -0500

    add jwt to connect socket

--- src/messages-ws/messages-ws.gateway.ts
+++ src/messages-ws/messages-ws.gateway.ts
@@ -1,21 +1,35 @@
 import {
   OnGatewayConnection,
   OnGatewayDisconnect,
+  SubscribeMessage,
   WebSocketGateway,
   WebSocketServer,
 } from '@nestjs/websockets'
 import { MessagesWsService } from './messages-ws.service'
 import { Server, Socket } from 'socket.io'
+import { JwtService } from '@nestjs/jwt'
 
 @WebSocketGateway({ cors: true })
 export class MessagesWsGateway
   implements OnGatewayConnection, OnGatewayDisconnect
 {
   @WebSocketServer() wss: Server
-  constructor(private readonly messagesWsService: MessagesWsService) {}
 
-  handleConnection(client: any) {
+  constructor(
+    private readonly messagesWsService: MessagesWsService,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  async handleConnection(client: any) {
     // console.log('handleConnection', client.id)
+    const token = client.handshake.headers.authorization
+    try {
+      const payload = await this.jwtService.verify(token)
+      console.log(payload)
+    } catch (error) {
+      client.disconnect()
+      return
+    }
     this.messagesWsService.registerClient(client)
 
     this.wss.emit(
@@ -24,12 +38,32 @@ export class MessagesWsGateway
     )
   }
   handleDisconnect(client: Socket) {
-    // console.log('handleDisconnect', client.id)
     this.messagesWsService.removeClient(client.id)
 
     this.wss.emit(
       'clients-updated',
       this.messagesWsService.getConnectedClients(),
     )
+  }
+
+  @SubscribeMessage('message-client')
+  handleMessageClient(client: Socket, payload: any): void {
+    //! Emite el mensaje unicamente al cliente que lo envia
+    // client.emit('message-server', {
+    //   message: payload.message + ' (from server)',
+    //   fullName: 'Soy yo',
+    // })
+
+    // ! Emite el mensaje a todos los clientes menos al cliente inicial
+    // client.broadcast.emit('message-server', {
+    //   message: payload.message + ' (from server)',
+    //   fullName: 'Soy yo a todos',
+    // })
+
+    // ! Emite el mensaje a todos los clientess
+    this.wss.emit('message-server', {
+      message: payload.message + ' (from server)',
+      fullName: client.id,
+    })
   }
 }
